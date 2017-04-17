b6f68c6868cb7aec3b19d8b151dcebfd6772fbcd
jflinter <jflinter11@gmail.com>
2017-04-17 15:20:01 -0400

    swift 3

--- PaintBucket/UIImage+PaintBucket.swift
+++ PaintBucket/UIImage+PaintBucket.swift
@@ -10,14 +10,15 @@ import UIKit
 
 public extension UIImage {
     
-    @objc public func pbk_imageByReplacingColorAt(x: Int, _ y: Int, withColor: UIColor, tolerance: Int, antialias: Bool = false) -> UIImage {
+    @objc public func pbk_imageByReplacingColorAt(_ x: Int, _ y: Int, withColor: UIColor, tolerance: Int, antialias: Bool = false) -> UIImage {
         let point = (x, y)
-        let imageBuffer = ImageBuffer(image: self.CGImage!)
-        let pixel = imageBuffer[imageBuffer.indexFrom(point)]
+        let imageBuffer = ImageBuffer(image: self.cgImage!)
+        let index = imageBuffer.indexFrom(x, y)
+        let pixel = imageBuffer[index]
         let replacementPixel = Pixel(color: withColor)
         imageBuffer.scanline_replaceColor(pixel, startingAtPoint: point, withColor: replacementPixel, tolerance: tolerance, antialias: antialias)
         
-        return UIImage(CGImage: imageBuffer.image, scale: self.scale, orientation: UIImageOrientation.Up)
+        return UIImage(cgImage: imageBuffer.image, scale: self.scale, orientation: UIImageOrientation.up)
     }
     
 }
